ffad65c588fcab9029bf51724ea9f900bda451ab
Sudha Rao <raosudha@umiacs.umd.edu>
2019-04-12 18:27:52 -0400

    Fixed issue in pagerank

--- src/pagerank.py
+++ src/pagerank.py
@@ -14,20 +14,20 @@ def add_edges_between_contexts(contexts_sim, G, N):
 			if i == j:
 				continue
 			if contexts_sim[i][j] > 0.5:
-				wt = 1 - contexts_sim[i][j]
+				wt = contexts_sim[i][j]
 				G.add_edge(i, j, weight=wt)
 				G.add_edge(j, i, weight=wt)
 
 
-def add_edges_between_questions(questions_sim, G, M, start, end):
+def add_edges_between_questions(questions_sim, ques_dict, ids, G, N, M, start, end):
 	# Adding edges between question nodes
 	for i in range(M):
 		for j in range(start, end):
 			if i == j:
 				continue
 			if questions_sim[i][j] > 0.5:
-				wt = 1 - questions_sim[i][j]
-				print ques_dict[ids[i]+'_'+str(i)], ques_dict[ids[j]+'_'+str(j)], wt
+				wt = questions_sim[i][j]
+				#print ques_dict[ids[i]+'_'+str(i)], ques_dict[ids[j]+'_'+str(j)], wt
 				G.add_edge(N+i, N+j, weight=wt)
 				G.add_edge(N+j, N+i, weight=wt)
 
@@ -48,8 +48,6 @@ def add_edges_between_context_question(ids, G, N):
 def main(args):
 	sparse_contexts_sim = scipy.sparse.load_npz(args.contexts_sim)
 	contexts_sim = sparse_contexts_sim.toarray()
-	print len(contexts_sim)
-	print len(contexts_sim[0])
 	ids = [line.strip('\n') for line in open(args.ids, 'r').readlines()]
 	i = 0
 	ques_dict = {}
@@ -67,7 +65,7 @@ def main(args):
 	contexts_sim = None	
 
 	questions_sim = scipy.sparse.load_npz(args.questions_sim_part1).toarray()
-	add_edges_between_questions(questions_sim, G, M, start=0, end=10000)
+	add_edges_between_questions(questions_sim, ques_dict, ids, G, N, M, start=0, end=10000)
 	questions_sim = None
 
 	#questions_sim = scipy.sparse.load_npz(args.questions_sim_part2).toarray()
